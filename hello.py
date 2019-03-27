073b7d1b9473791c2d5e6e8f0a643686a6e0c6b1
parkjunhyeong1995 <48742443+parkjunhyeong1995@users.noreply.github.com>
2019-03-27 11:13:07 +0900

    GitHub for Git pull

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 // For command line git tutorial
 // First: Github modification
 // Second: Local repository modification
+
 print("Heiio World!")
 print("Tell your world")
 print("tell his world")
 print("Tell her world")
+print("Tell my world")

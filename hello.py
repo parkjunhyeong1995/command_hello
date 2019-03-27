ee926ed22309307f1fe85ef6d1b615cba8fc489d
mskim <rakuri@naver.com>
2019-03-27 11:01:13 +0900

    conflict resolved Github

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 // For command line git tutorial
 // First: Github modification
 // Second: Local repository modification
-print("Heiio World!">
+print("Heiio World!")
 print("Tell your world")
 print("tell his world")
 print("Tell her world")
